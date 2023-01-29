8028b300a022e707209a67a94318ac3ebdcb9508
jjusticz <jjusticz@uchicago.edu>
2023-01-29 22:42:16 +0000

    Final commit

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,4 +1,4 @@
-class FireController < ApplicationController
+class FortunesController < ApplicationController
 
   def horoscopes
     @name = params.fetch("the_sign")
@@ -6,8 +6,16 @@ class FireController < ApplicationController
     this_zodiac = all_zodiacs.fetch(@name.to_sym)
     @horoscope = this_zodiac.fetch(:horoscope)
 
+    @array_of_numbers = Array.new
+
+    5.times do
+      another_number = rand(1...100)
+      
+      @array_of_numbers.push(another_number)
+    end
+    
     render({:template => "layouts/fortune.html.erb"})
-  
+    
   end
 
 end
